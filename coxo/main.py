9c341c5f9ad8c703d05c0ea4b4109b2494adf811
Zac Todd <zactodd0@gmail.com>
2021-12-10 12:09:39 +1300

    Update main.py

--- coxo/main.py
+++ coxo/main.py
@@ -7,6 +7,11 @@ def is_win(board):
     return board[0][0][1] == board[1][1][1] == board[2][2][1] is not None or \
            board[0][2][1] == board[1][1][1] == board[2][0][1] is not None
 
+
+def moves(board, turn):
+    return (i, j for i, r in enumerate(board) for j, (v, p) in enumerate(r) if turn > v)
+
+
 # def dfs(board=None, turn=0, max_depth=10, depth=0, alpha=-1000000, beta=1000000):
 #     if board is None:
 #         board = [
